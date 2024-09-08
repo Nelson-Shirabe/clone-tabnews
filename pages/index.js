6ae22f98136329ff554e43e66aefc1d78acbb27a
Nelson Shirane <137460733+Nelson-Shirabe@users.noreply.github.com>
2024-09-08 23:49:09 +0000

    mudando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mozão Te amo! Se você conseguiu ver essa mensagem manda um coração no wpp =)</h1>
+    return <h1>Mozão Te amo! Se você conseguiu ver essa mensagem manda um coração no whats =) ❤️ </h1>
 }
 
 export default Home;
